8ae91e8e7fcb95a3c02f03f7550b3495132d0f83
nnickd <nicolas.dellitalia@gmail.com>
2025-12-24 12:30:25 -0500

    prep for prod deployment

--- server/HackerNewsChallenge.Api/Program.cs
+++ server/HackerNewsChallenge.Api/Program.cs
@@ -1,23 +1,34 @@
 using HackerNewsChallenge.Api.Configuration;
 using HackerNewsChallenge.Api.Services;
 using HackerNewsChallenge.Api.Services.Interfaces;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddMemoryCache();
 
 builder.Services.Configure<HackerNewsOptions>(
     builder.Configuration.GetSection(HackerNewsOptions.SectionName));
 
 builder.Services.AddHttpClient<IHackerNewsClient, HackerNewsClient>();
-
 builder.Services.AddScoped<IStoryService, StoryService>();
 
 var app = builder.Build();
 
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseHttpsRedirection();
+
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
+
 app.Run();
